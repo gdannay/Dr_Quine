05bce9eaa901d11586693b2bd8d86ee9c7635e51
gdannay <gregory.dannay@sciencespo.fr>
2018-04-08 10:57:03 +0000

    Colleen

---
+++ C/Colleen.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+	Com
+*/
+void test(){char *a="#include <stdio.h>%c/*%c%cCom%c*/%cvoid test(){char *a=%c%s%c;printf(a, 10, 10, 9, 10, 10, 34, a, 34, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 10);}%cint main(){%c%c/*%c%c%cAutre com%c%c*/%c%ctest();}%c";printf(a, 10, 10, 9, 10, 10, 34, a, 34, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 10);}
+int main(){
+	/*
+		Autre com
+	*/
+	test();}
